7d70048b980a97a8ae027ed20b17a57aa21bb9f7
linhan <yonglin12@hotmail.com>
2016-08-27 14:34:53 +0800

    Add value attribute to Address class to make AdaptedPerson class work, add additional constructor

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -2,7 +2,6 @@ package seedu.addressbook.data.person;
 
 import java.util.Objects;
 
-import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.person.address.Block;
 import seedu.addressbook.data.person.address.PostalCode;
 import seedu.addressbook.data.person.address.Street;
@@ -23,7 +22,7 @@ public class Address {
     private Unit unit;
     private PostalCode postalCode;
     private boolean isPrivate;
-    
+    public String value;
     /**
      * Assumption: Every field must be present and not null.
      * Input entered in the order BLOCK, STREET, UNIT, POSTAL_CODE
@@ -34,6 +33,17 @@ public class Address {
         this.street = street;
         this.unit = unit;
         this.postalCode = postalCode;
+        value = buildAddressString();
+    }
+    
+    public Address(String value, boolean isPrivate) {
+        this.isPrivate = isPrivate;
+        this.value = value;
+        String[] Address = value.split(", ");
+        block = new Block(Address[0]);
+        street = new Street(Address[1]);
+        unit = new Unit(Address[2]);
+        postalCode = new PostalCode(Address[3]);
     }
     
     public Block getBlock() {
@@ -54,15 +64,7 @@ public class Address {
 
     @Override
     public String toString() {
-        final StringBuilder builder = new StringBuilder();
-        builder.append(getBlock())
-               .append(", ")
-               .append(getStreet())
-               .append(", ")
-               .append(getUnit())
-               .append(", ")
-               .append(getPostalCode());
-        return builder.toString();
+        return value;
     }
 
     @Override
@@ -82,6 +84,18 @@ public class Address {
     
     public boolean isPrivate() {
         return isPrivate;
+    }
+    
+    public String buildAddressString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getBlock())
+               .append(", ")
+               .append(getStreet())
+               .append(", ")
+               .append(getUnit())
+               .append(", ")
+               .append(getPostalCode());
+        return builder.toString();
     }
     
 }
