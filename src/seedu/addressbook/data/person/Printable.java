c90528f8fadb959db322fa5f3401d054651c1358
linhan <yonglin12@hotmail.com>
2016-09-02 13:34:44 +0800

    Add Printable interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,10 @@
+package seedu.addressbook.data.person;
+
+/**
+ * An interface for a Printable contact detail for a Person in the addressbook.
+ */
+public interface Printable {
+    
+    String getPrintableString();
+
+}
