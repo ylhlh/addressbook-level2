2a76ecc3a9bae42e8b04be1532ca43582af6d488
linhan <yonglin12@hotmail.com>
2016-09-03 12:38:46 +0800

    Add instance and static variables to keep track of sequence numbers of Persons

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -14,7 +14,9 @@ public class Person implements ReadOnlyPerson {
     private Phone phone;
     private Email email;
     private Address address;
-
+    private int sequenceNumber;
+    private static int nextSequenceNumber = 0;
+    
     private final UniqueTagList tags;
     /**
      * Assumption: Every field must be present and not null.
@@ -25,6 +27,7 @@ public class Person implements ReadOnlyPerson {
         this.email = email;
         this.address = address;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+        this.sequenceNumber = ++nextSequenceNumber;
     }
 
     /**
